7f098b28e84fffecb82403139bc9ac0af3e836b3
doddyz <doddyz@live.com>
2023-01-26 01:01:03 +0000

    FR w Search Kwd Feature

--- pages/fr.py
+++ pages/fr.py
@@ -5,13 +5,30 @@ from fun import *
 page_lang = 'fr-FR'
 
 # Preselect World topic
+
+sources_container = st.sidebar.empty()
+
+search_term = st.sidebar.text_input('Search')
+
 topic = st.sidebar.selectbox('News topic', sorted(list(TOPIC_IDS.keys())), 2)
 
-feed = feedparser.parse(create_google_news_topic_rss_url(topic, page_lang))
 
-sources = get_feed_available_sources(feed)
 
-# Preselect Guardian as default sourcew
-source = st.sidebar.selectbox('News source', sources, 3)
 
-create_markdown_news_topic_section(topic, feed, source)
+# create_markdown_news_topic_section(topic, feed, source)
+
+if (len(search_term) > 0):
+    
+    feed = feedparser.parse(create_google_news_search_rss_url(search_term, page_lang))
+    sources = get_feed_available_sources(feed)
+    source = sources_container.selectbox('News source', sources, 3)
+
+    create_markdown_news(search_term, feed, source)
+
+else:
+    
+    feed = feedparser.parse(create_google_news_topic_rss_url(topic, page_lang))
+    sources = get_feed_available_sources(feed)
+    source = sources_container.selectbox('News source', sources, 3)
+
+    create_markdown_news(topic, feed, source)
