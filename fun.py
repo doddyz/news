98cab35ba3afdc83c4983277444e2a68df74046f
doddyz <doddyz@live.com>
2023-01-29 11:53:56 +0000

    GB w Radio Source Options

--- fun.py
+++ fun.py
@@ -1,32 +1,57 @@
-# Next finalise G News options by testing custom topics like tennis, maths, marketing related etc ...
-# L'information et l'anticipation, make this one private, see which one is currently private
+# Resolve Sources selection issue: either with checkbox or else as variables in bg
+# See how to integrate sub topics cleanly, either as extra options in the topics selector. Create a tabbed approach if pages don't work well
+# Make all pages show same widgets, search for all etc...
+# Redirect to fr page by default or hide homepage 
 
 
 import feedparser
 import streamlit as st
 
-# https://news.google.com/rss
+BASE_RSS_HOME_URL = 'https://news.google.com/rss'
+
 BASE_RSS_TOPIC_URL = 'https://news.google.com/rss/topics/'
 
 BASE_RSS_SEARCH_URL = 'https://news.google.com/rss/search?q='
 
-PREFERRED_SOURCES = {}
+PREFERRED_SOURCES = {
+
+    'gb': ['BBC', 'The Guardian', 'The Independent', 'Evening Standard']
+    
+}
 
 TOPIC_IDS = {
-    
+        
+    'Business': 'CAAqKggKIiRDQkFTRlFvSUwyMHZNRGx6TVdZU0JXVnVMVWRDR2dKSFFpZ0FQAQ',
+    'Local': 'CAAqHAgKIhZDQklTQ2pvSWJHOWpZV3hmZGpJb0FBUAE',
+    'Science': 'CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp0Y1RjU0JXVnVMVWRDR2dKSFFpZ0FQAQ',
+    'Sports' : 'CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp1ZEdvU0JXVnVMVWRDR2dKSFFpZ0FQAQ',
+    'Technology': 'CAAqKggKIiRDQkFTRlFvSUwyMHZNRGRqTVhZU0JXVnVMVWRDR2dKSFFpZ0FQAQ',
+    'Top Stories' : 'CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSFFpZ0FQAQ',
     'United Kingdom' : 'CAAqJQgKIh9DQkFTRVFvSUwyMHZNRGR6YzJNU0JXVnVMVWRDS0FBUAE',
-    'World' : 'CAAqKggKIiRDQkFTRlFvSUwyMHZNRGx1YlY4U0JXVnVMVWRDR2dKSFFpZ0FQAQ',
-    'Sports' : 'CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp1ZEdvU0JXVnVMVWRDR2dKSFFpZ0FQAQ'
+    'World' : 'CAAqKggKIiRDQkFTRlFvSUwyMHZNRGx1YlY4U0JXVnVMVWRDR2dKSFFpZ0FQAQ'
     
     }
+
+TOPIC_SECTION_IDS = {
+
+    'Tennis' : 'CAQiSkNCQVNNUW9JTDIwdk1EWnVkR29TQldWdUxVZENHZ0pIUWlJT0NBUWFDZ29JTDIwdk1EZGljekFxQ2dvSUVnWlVaVzV1YVhNb0FBKi4IACoqCAoiJENCQVNGUW9JTDIwdk1EWnVkR29TQldWdUxVZENHZ0pIUWlnQVABUAE'
+
+    }
+
+def create_google_news_home_rss_url(lang):
+    return BASE_RSS_HOME_URL + '?hl=' + lang
 
 
 def create_google_news_topic_rss_url(topic, lang):
     return BASE_RSS_TOPIC_URL + TOPIC_IDS[topic] + '?hl=' + lang
 
 
+def create_google_news_topic_section_rss_url(topic, section, lang):
+    return BASE_RSS_TOPIC_URL + '/topics/' + TOPIC_SECTION_IDS[section] + '?hl=' + lang
+
+
 def create_google_news_search_rss_url(search_term, lang):
-    return BASE_RSS_SEARCH_URL + search_term + '&hl=' + lang
+    return BASE_RSS_SEARCH_URL + search_term.replace(' ', '%20') + '&hl=' + lang
 
 
 def get_feed_available_sources(feed):
--- pages/3_🇬🇧_gb.py
+++ pages/3_🇬🇧_gb.py
@@ -5,13 +5,14 @@ from fun import *
 page_lang = 'en-GB'
 
 # Preselect World topic
-topic = st.sidebar.selectbox('News topic', sorted(list(TOPIC_IDS.keys())), 2)
+topic = st.sidebar.selectbox('News topic', sorted(list(TOPIC_IDS.keys())), 5)
 
 feed = feedparser.parse(create_google_news_topic_rss_url(topic, page_lang))
 
 sources = get_feed_available_sources(feed)
 
 # Preselect Guardian as default sourcew
-source = st.sidebar.selectbox('News source', sources, 3)
+# source = st.sidebar.selectbox('News source', sources, 3)
+source = st.sidebar.radio('News source', PREFERRED_SOURCES['gb'])
 
 create_markdown_news_topic_section(topic, feed, source)
